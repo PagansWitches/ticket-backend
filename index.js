ad80c7de4bfc4513f9c6f38fd60d22802d9a9c5a
PagansWitches <tickets@afestivalforpagansandwitches.co.uk>
2025-05-11 22:10:29 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const Stripe = require('stripe');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
+app.get('/pay', async (req, res) => {
+  const { email, ...tickets } = req.query;
+  const items = [];
+
+  const priceMap = {
+    "3DayTicket": "price_1RNbaORsTRQij9eYlnTNO5YA",
+    "AdultSaturdayTicket": "price_1RNbb3RsTRQij9eYgKmUWrK9",
+    "AdultSundayTicket": "price_1RNbbjRsTRQij9eY0k1Tk2At",
+    "AdultMondayTicket": "price_1RNbcURsTRQij9eYGyGXw1t2",
+    "ChildSaturdayTicket": "price_1RNbe6RsTRQij9eYFCd9O53y",
+    "ChildSundayTicket": "price_1RNbevRsTRQij9eY1lGOztqg",
+    "ChildMondayTicket": "price_1RNbfHRsTRQij9eYPm1g9chR",
+    "Disabled3DayTicket": "price_1RNbgSRsTRQij9eYGdVIPFvy",
+    "DisabledSaturdayTicket": "price_1RNbhDRsTRQij9eYSklKbIRI",
+    "DisabledSundayTicket": "price_1RNbhXRsTRQij9eYOOUfpSb1",
+    "DisabledMondayTicket": "price_1RNbhsRsTRQij9eYlNCbmq0y"
+  };
+
+  for (const [key, value] of Object.entries(tickets)) {
+    if (priceMap[key]) {
+      items.push({
+        price: priceMap[key],
+        quantity: parseInt(value)
+      });
+    }
+  }
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    mode: 'payment',
+    customer_email: email,
+    line_items: items,
+    success_url: 'https://yourwebsite.com/success',
+    cancel_url: 'https://yourwebsite.com/cancel'
+  });
+
+  res.redirect(303, session.url);
+});
+
+app.listen(3000, () => console.log("Ticket server running on port 3000"));
